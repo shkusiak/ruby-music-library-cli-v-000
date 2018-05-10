ceb0a9221a2b4344e857810c7722c731523eca04
Shea Kusiak <shkusiak@gmail.com>
2018-05-10 01:32:30 +0000

    automatically backed up by learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,66 @@
+class MusicLibraryController
+  def initialize(path = "./db/mp3s")
+    importer = MusicImporter.new(path)
+    importer.import
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+
+    loop do
+      input = gets.strip #until input = "exit"
+      break if input == "exit"
+
+      if input == "list artists"
+        list_artists
+      elsif input == "list songs"
+        list_songs
+      elsif input == "list genres"
+        list_genres
+      elsif input == "list artist"
+        list_songs_by_artist
+      elsif input == "list genre"
+        list_songs_by_genre
+      elsif input == "play song"
+        play_song
+      end
+    end
+  end
+
+  def list_songs
+    Song.all.sort{ |x, y| x.name <=> y.name}.each_with_index{ |song, i|
+      puts "#{i+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    }
+  end
+
+  def list_artists
+    Artist.all.sort { |x,y| x.name <=> y.name}.each_with_index{ |artist,i|
+      puts "#{i+1}. #{artist.name}"
+    }
+  end
+
+  def list_genres
+    Genre.all.sort { |x,y| x.name <=> y.name}.each_with_index{ |genre,i|
+      puts "#{i+1}. #{genre.name}"
+    }
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+  end
+
+  def list_songs_by_genre
+  end
+
+  def play_song
+  end
+
+end
