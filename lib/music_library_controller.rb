13e70c1e35beebda9bd23345e92d0dcf08aed330
Shea Kusiak <shkusiak@gmail.com>
2018-05-10 01:44:40 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -55,7 +55,7 @@ class MusicLibraryController
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    
+
     input = gets.strip
 
     if artist = Artist.find_by_name(input)
@@ -63,12 +63,12 @@ class MusicLibraryController
         puts "#{i+1}. #{song.name} - #{song.genre.name}"
       }
     end
-    
+
   end
 
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
-    
+
     input = gets.strip
 
     if genre = Genre.find_by_name(input)
@@ -76,15 +76,15 @@ class MusicLibraryController
         puts "#{i+1}. #{song.artist.name} - #{song.name}"
       }
     end
-    
+
   end
 
   def play_song
     puts "Which song number would you like to play?"
-    
+
     input = gets.strip
     if input.between?(1,list_songs.size)
-      
+
     end
   end
 
